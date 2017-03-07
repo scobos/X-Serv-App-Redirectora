9ebdb4df1bedd492ca331f1809b1d610d0c8ff85
Sergio Cobos <SergioCobos@MacBook-Pro-de-Cobos.local>
2017-03-07 19:10:22 +0100

    Ejercicio 14.4

--- redirectora.py
+++ redirectora.py
@@ -6,12 +6,6 @@ class redirect(webapp.webApp):
         numero = random.randint(1,100000000)
         return ("303 See Other", "<html><body><a href= '"+
                 str(numero)+"'>Clic aqui para redirigirte</a></body></html>")
-
-
-#        return ("303 See Other", "<html><head><meta http-equiv='Refresh' " +
-#                        "content='5;url=" + str(randomURL) + "'></head>" +
-#                        "<body><p>Seras redireccionado en 5 segundos o si haces click " +
-#                        "<a href='" + str(randomURL) + "'>aqui</a></p></body></html>")
-
+                
 if __name__ == "__main__":
     testRedirecting = redirect("localhost", 1234)
